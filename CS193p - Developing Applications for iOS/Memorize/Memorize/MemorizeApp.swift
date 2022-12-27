565fc932b1a12bcf52210c6e6b2c8de9b6e99354
tommya98 <tommya98@naver.com>
2022-12-27 22:12:38 +0900

    CS193p first commit

---
+++ CS193p - Developing Applications for iOS/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 엄석훈 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
