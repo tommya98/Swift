b2874805646d7927ea60828180c19e3e35ad5e67
tommya98 <tommya98@naver.com>
2023-01-03 15:02:48 +0900

    꼼꼼한 재은 씨 Chapter 2

---
+++ 꼼꼼한 재은 씨의 Swift - 문법편/Chapter 2/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 엄석훈 on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
